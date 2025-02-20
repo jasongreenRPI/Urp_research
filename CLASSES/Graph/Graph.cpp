c3ac17f510b7302b2b9a2dd947c991c1c9190fed
jasongreenb <greenberg.jason2004@gmail.com>
2025-02-19 20:30:12 -0500

    pushing new changes

--- CLASSES/Graph/Graph.cpp
+++ CLASSES/Graph/Graph.cpp
@@ -47,7 +47,6 @@ void Graph<T>::removeVertex(const T& vertex) {
 }
 
 template <typename T>
-<<<<<<< HEAD
 void Graph<T>::addEdge(const T& from, const T& to, int weight = 1) {
     // Validate source vertex exists
     if (!hasVertex(from)) {
@@ -181,65 +180,4 @@ size_t Graph<T>::getEdgeCount() const {
     // Since this is an undirected graph, each edge is counted twice
     // (once for each direction), so we divide by 2
     return totalEdges / 2;
-=======
-void Graph<T>::removeEdge(const T& from, const T& to) {
-    // Check if both vertices exist
-    if (!hasVertex(from) || !hasVertex(to)) {
-        error << "removeEdge: One or both vertices do not exist in graph." << endl;
-        return;
-    }
-
-    // Remove edge in both directions (since it's an undirected graph)
-    // Remove to from from's neighbor list
-    adjacencyList[from].erase(
-        remove_if(adjacencyList[from].begin(), adjacencyList[from].end(),
-            [&](const pair<T, int>& edge) {
-                return edge.first == to;
-            }),
-        adjacencyList[from].end()
-    );
-
-    // Remove from from to's neighbor list
-    adjacencyList[to].erase(
-        remove_if(adjacencyList[to].begin(), adjacencyList[to].end(),
-            [&](const pair<T, int>& edge) {
-                return edge.first == from;
-            }),
-        adjacencyList[to].end()
-    );
-}
-
-// Args: 
-// from: source vertex
-// to: destination vertex
-template <typename T>
-int Graph<T>::getEdgeWeight(const T& from, const T& to) const {
-    if (!hasVertex(from) || !hasVertex(to)) {
-        error << "getEdgeWeight: One or both vertices do not exist in graph." << endl;
-        return 0;
-    }
-    
-    // Find the edge in from's neighbor list
-    // Use reference to avoid copying the vector
-    const auto& neighbors = adjacencyList.at(from);
-
-    // find_if algorithm
-    // Function: searches through the range (begin to end)
-    // Return: an iterator to the first element for which the lambda function returns true
-    // If no such element is found, the function returns end()
-    auto it = find_if(neighbors.begin(), neighbors.end(),
-        // lambda function: returns true if the currect edge's destination vertex equals our target vertex
-        [&](const pair<T, int>& edge) {
-            return edge.first == to;
-        });
-    
-    // Check if edge exists in the graph
-    if (it == neighbors.end()) {
-        error << "getEdgeWeight: Edge does not exist in graph." << endl;
-        return 0;
-    }
-    
-    // Return the weight of the edge
-    return it->second;
->>>>>>> f7991f910cdcf44b1a5a3234d6c65f08332eb589
 }
